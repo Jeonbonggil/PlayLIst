c6458729f001dc2dcc7f07ca4ccf28faa3b85a08
bgjeon <bgjeon@lotte.net>
2024-05-16 13:11:30 +0900

    CHORE: Appdelegate 폴더 이동 및 TrackDetail 상세페이지 생성

---
+++ TrackDetail/View/TrackDetailViewController.swift
@@ -0,0 +1,10 @@
+//
+//  TrackDetailViewController.swift
+//  Playlist
+//
+//  Created by ec-jbg on 5/16/24.
+//
+
+import UIKit
+final class TrackDetailViewController: UIViewController, StoryboardView {
+}
